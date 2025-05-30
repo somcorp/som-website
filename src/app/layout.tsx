0dd34324a8efe2f931e936c88dc51840e6271dc0
Carlos Eduardo <kadufenix10@gmail.com>
2025-05-30 02:32:14 -0300

    feat: add analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Orbitron } from "next/font/google";
 import "./globals.css";
 
@@ -21,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased`}>{children}</body>
+      <body className={`${poppins.className} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
